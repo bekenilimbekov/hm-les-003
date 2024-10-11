35f5ea2248a461101efedf537ac088c0f4b8bc2a
bekenilimbekov <beken28_12@icloud.com>
2024-10-12 00:15:01 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+// const a = document.getElementById("button")
+// console.log(a)
+
+// a.addEventListener('click',function(){
+//     a.innerText = ("Go")
+// })
+
+// const text = document.querySelector(".text")
+
+// const show = document.getElementById("show")
+// console.log(show);
+// show.addEventListener('click', function(){
+//     text.style.display = 'block';
+// })
+
+// const remove = document.getElementById("remove")
+// console.log(remove)
+// remove.addEventListener('click', function(){
+//     text.style.display = 'none';
+// })
+
+//  hw-1. Изменение текста в инпуте:  
+// Найдите поле ввода с id inputField и кнопку.Когда пользователь нажимает на кнопку, текст в поле ввода должен изменяться на "Привет, мир!".
+
+// const a = document.getElementById("button")
+
+// a.addEventListener('click', function(){
+//     const b = document.getElementById("inputField") .value = "hi world!"
+
+//     console.log(a,b)
+// })
+
+//hw3. Подсчёт количества кликов:  
+// Реализуйте кнопку, которая при каждом клике увеличивает счётчик кликов и отображает количество кликов на странице.
+
+// const plus = document.getElementById("button")
+// const num = document.getElementById("result")
+// console.log(plus)
+// console.log(num)
+
+
+// plus.addEventListener('click', function(){
+
+//     num.innerHTML = (num)
+
+// })
+// hw-2. Изменение стиля элемента:  
+// Реализуйте кнопку, которая при нажатии изменяет стиль текста абзаца — делает его жирным и увеличивает размер шрифта.
+
+// const text = document.querySelector(".title")
+// const button = document.querySelector(".button")
+// console.log(text)
+// console.log(button)
+
+// button.addEventListener('click', function(){
+//     text.innerText = ("DODGE CHALLENGER")
+//     text.style.color = "red"
+// })
+
+// calkulater
+
+// function func() {
+//     let num1 = Number(document.getElementById("num1").value)
+//     let num2 = Number(document.getElementById("num2").value)
+//     document.getElementById("result").innerHTML = result;
+    
+//     switch (op) {
+//         case '+':
+//             result = num1 + num2
+//             break;
+//         case '-':
+//             result = num1 - num2
+//             break;
+//         case '*':
+//             result = num1 * num2
+//             break;
+//         case '/':
+//             result = num1 / num2
+//             break;
+//     }
+// }
+
+
+
